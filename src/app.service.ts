afda9b3ca52464068e195c9a9aa9f73cfb318ab0
KingKiarie <kiarie7mbugua@gmail.com>
2025-06-21 21:46:47 +0300

    chore: updated validation pipe globally

--- src/app.service.ts
+++ src/app.service.ts
@@ -3,6 +3,6 @@ import { Injectable } from '@nestjs/common';
 @Injectable()
 export class AppService {
   getHello(): string {
-    return 'Building Emma-LBD blog station';
+    return 'Building Emma-LBD blog';
   }
 }
